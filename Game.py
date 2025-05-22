a9f0e298b9470cf389b2734cc7d9c2b4c50cc0cf
Cracksmity <cracksmity@gmail.com>
2025-05-22 12:37:58 -0600

    Jugador Agregado

--- Game.py
+++ Game.py
@@ -11,12 +11,27 @@ pygame.display.set_caption("Invasion Espacial")
 icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
 
+# Jugador
+
+img_jugador = pygame.image.load("nave.png")
+jugador_x = 330
+jugador_y = 450
+
+def jugador(x, y):
+    pantalla.blit(img_jugador, (x, y))
+
 # Bucle principal
-ejectutar = True
-while ejectutar:
+ejecutar = True
+while ejecutar:
+
+    # RGB
+    pantalla.fill((205, 144, 228))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            ejectutar = False
+            ejecutar = False
 
-    pantalla.fill((205, 144, 228))
+
+    jugador(jugador_x, jugador_y)
+
     pygame.display.update()
