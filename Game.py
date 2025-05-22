dcd2898045edd8c5dcf8388963e730a2cb3b00d6
Cracksmity <cracksmity@gmail.com>
2025-05-22 13:52:29 -0600

    Municion Agregada

--- Game.py
+++ Game.py
@@ -29,6 +29,14 @@ enemigo_y = random.randint(50, 200)
 enemigo_x_cambio = 0.3
 enemigo_y_cambio = 30
 
+# Municion
+img_municion = pygame.image.load("bala.png")
+bala_x = 0
+bala_y = 536
+bala_x_cambio = 0
+bala_y_cambio = .2
+bala_visible = False
+
 # Funcion para dibujar el jugador
 def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
@@ -36,6 +44,12 @@ def jugador(x, y):
 # Funcion para dibujar el enemigo
 def enemigo(x, y):
     pantalla.blit(img_enemigo, (x, y))
+
+# Funcion para disparar la municion
+def disparar_municion(x, y):
+    global bala_visible
+    bala_visible = True
+    pantalla.blit(img_municion, (x, y))
 
 # Bucle principal
 ejecutar = True
@@ -58,6 +72,12 @@ while ejecutar:
                 jugador_x_cambio -= .25
             if event.key == pygame.K_RIGHT:
                 jugador_x_cambio += .25
+            if event.key == pygame.K_SPACE:
+                if not bala_visible:
+                    bala_x = jugador_x + (img_jugador.get_width() // 2) - (
+                                img_municion.get_width() // 2)  # Centra la bala horizontalmente
+                    bala_y = jugador_y  # La bala aparecerá desde la nave
+                    bala_visible = True
 
         # Soltar teclas
         if event.type == pygame.KEYUP:
@@ -85,6 +105,16 @@ while ejecutar:
     elif enemigo_x >= 758:
         enemigo_x_cambio = -0.1
         enemigo_y += enemigo_y_cambio
+
+    # Movimiento de la municion
+    if bala_visible:
+        bala_y -= bala_y_cambio
+        if bala_y <= 0:  # Si la bala llega al tope de la pantalla
+            bala_visible = False  # La bala deja de ser visible
+            bala_y = jugador_y  # Reset de la posición Y de la bala
+            bala_x = jugador_x  # Reset de la posición X de la bala
+        else:
+            disparar_municion(bala_x, bala_y)
 
     jugador(jugador_x, jugador_y)
     enemigo(enemigo_x, enemigo_y)
