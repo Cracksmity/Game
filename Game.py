25103454ea8c1d85d31fba939258e5f895634c29
Cracksmity <cracksmity@gmail.com>
2025-05-22 12:02:48 -0600

    Primer Commit

---
+++ Game.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+pantalla = pygame.display.set_mode((800, 600))
+
+ejectutar = True
+while ejectutar:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ejectutar = False
