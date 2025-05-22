1029e6f18f0ba8e08d1c71883e6f46885b0805bc
Cracksmity <cracksmity@gmail.com>
2025-05-22 12:50:58 -0600

    Movilidad Agregada

--- Game.py
+++ Game.py
@@ -16,6 +16,8 @@ pygame.display.set_icon(icono)
 img_jugador = pygame.image.load("nave.png")
 jugador_x = 330
 jugador_y = 450
+jugador_x_cambio = 0
+jugador_y_cambio = 0
 
 def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
@@ -31,7 +33,20 @@ while ejecutar:
         if event.type == pygame.QUIT:
             ejecutar = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                jugador_x_cambio -= .25
+            if event.key == pygame.K_RIGHT:
+                jugador_x_cambio += .25
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0
+
+
+
+
+    jugador_x += jugador_x_cambio
     jugador(jugador_x, jugador_y)
 
     pygame.display.update()
